f7490eddda9b96cb4b4c8332bbd40c63351ae8ea
Rima Akter <rima92373@gmail.com>
2022-02-11 23:29:08 +0600

    add-tags

---
+++ conversion.js
@@ -0,0 +1,25 @@
+var firstName = 'Rima';
+var lastName = 'Akter';
+var fullName = firstName +' ' + lastName;
+console.log(fullName);
+
+var onionPrice = 23;
+var eggPrice = '23'
+var totalPrice =  onionPrice + eggPrice;
+console.log(totalPrice);
+
+var first = 'Rima';
+var last = 'Akter';
+var name = first +' ' +last;
+console.log(name);
+
+// integer
+var sunglass = 2;
+// float 
+var price = 99.99;
+var onionPrice = '23.22';
+var eggPrice = '22.3';
+var onionPriceNumber = parseInt(onionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+console.log(onionPriceNumber);
+console.log(eggPriceNumber);
---
+++ math-shorthand.js
@@ -0,0 +1,24 @@
+var price1 = 20;
+var price2 = 30;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(division);
+
+var doublePrice = price2*3;
+price1 = price1 + 20;
+// or price1 += 20;
+//or var newPrice = price1 + 10;
+// console.log(newPrice);
+console.log(doublePrice);
+console.log(price1);
+
+var age = 23;
+// age = age + 1;
+// age += 1;
+age++;
+console.log(age);
