9b31867b617a15cbfdfc12a867228cca8a3c92d0
Rima Akter <rima92373@gmail.com>
2022-02-11 22:03:26 +0600

    add-tags

---
+++ mathmetical-operation.js
@@ -0,0 +1,23 @@
+var onionPrice = 23;
+var eggPrice = 5;
+// multiplication
+var eggQuantity = 7;
+var eggPriceMultiplication = eggPrice * eggQuantity;
+console.log(eggPriceMultiplication);
+console.log(onionPrice);
+console.log(eggPrice);
+
+// addition
+console.log(onionPrice + eggPrice);
+// or var totalPrice=onionPrice+eggPrice;
+// console.log(totalPrice);
+
+// subtraction
+var priceDifference = eggPrice - onionPrice;
+console.log(priceDifference);
+
+// division
+var moneyAvailable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvailable / orangePrice;
+console.log(orangeQuantity);
