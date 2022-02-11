5a72aeb5360d3da168f7c819f0f54524dbe35e79
Rima Akter <rima92373@gmail.com>
2022-02-12 00:17:32 +0600

    modify

---
+++ variable-types.js
@@ -0,0 +1,20 @@
+var tShirtPrice = 200;
+console.log(typeof tShirtPrice);
+var tShirtPrice2 = '200';
+console.log(typeof tShirtPrice2);
+var isRaining = true;
+console.log(typeof isRaining);
+var isTaining;
+console.log(typeof isTaining);
+
+var today = new Date();
+console.log(typeof today);
+// console.log(today);
+
+// special edition
+var firstNumber = 0.2;
+var secondNumber  = 0.3;
+var total = firstNumber + secondNumber;
+// total = total.toFixed(2);
+// total = parseFloat(total);
+console.log(total);
