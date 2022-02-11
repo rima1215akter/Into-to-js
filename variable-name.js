59df5dc5d950540d039c719e26d728e766c88d80
Rima Akter <rima92373@gmail.com>
2022-02-11 21:30:38 +0600

    initial-commit

---
+++ variable-name.js
@@ -0,0 +1,20 @@
+var name = "Hero Alom";
+// reserved keyword not allowed
+var false = 12;
+var delete = 'delete';
+ 
+// name should be one word
+var nayok name = "Tom Jerry";
+
+// quote is not allowed
+var "name" = 'Rima Akter';
+// not allowed 
+var 99price = 25;
+var userhomeaddress = 'mirpur';
+var user-home-address = 'mirpur';
+var uha = 'mirpur';
+// allowed
+var userHomeAddress = 'mirpur';
+// rear use 
+var PIE = 3.14;
+var user_home_address = 'mirpur';
---
+++ variable.js
@@ -0,0 +1,10 @@
+// numeric variable
+var price = 21;
+var age = 23;
+var temperature = 37;
+// duble ("")//string variable
+var name = 'Rima Akter';
+var address = "mohakhali iph mosque";
+// boolean variable
+var pass = true;
+var subscribe = false;
