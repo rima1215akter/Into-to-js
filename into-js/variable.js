359f741574ca809e2c3677e50b693025280d9726
Rima Akter <90492917+rima1215akter@users.noreply.github.com>
2022-04-11 06:26:24 +0600

    modify

--- into-js/variable.js
+++ into-js/variable.js
@@ -2,9 +2,10 @@
 var price = 21;
 var age = 23;
 var temperature = 37;
+var price2 = 55;
 // double ("")//string variable
 var name = 'Rima Akter';
-var address = "mohakhali iph mosque";
+var address = "mohakhali IPH mosque";
 // boolean variable
 var pass = true;
 var subscribe = false;
